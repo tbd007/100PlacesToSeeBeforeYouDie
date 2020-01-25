ed3b544724cb9b8ed6a19016f2b62d87065504cd
tbd007 <58664635+tbd007@users.noreply.github.com>
2020-01-25 17:12:42 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,82 @@
+function random_imglink(){
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+var images = new Array
+images[1] = "https://www.farecompare.com/wp-content/uploads/2018/12/2019-Top-Travel-Destinations-Singapore.jpg",
+images[2] = "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1171238184%2F960x0.jpg%3Ffit%3Dscale",
+images[3] = "https://i.pinimg.com/originals/6d/01/a1/6d01a1cbb7a172df77bef22e7dbb5402.png",
+
+document.getElementById("img1").src = images[randomCount];
+
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;  
+var imagestwo=new Array
+imagestwo[1] = "http://www.world-around.me/wp-content/uploads/2016/04/top-destination-1170x600.jpg",  
+imagestwo[2] = "https://www.traveller.com.au/content/dam/images/g/h/d/f/b/p/image.gallery.galleryLandscape.620x414.ghgq96.png/1433489931758.jpg", 
+imagestwo[3] = "https://media1.popsugar-assets.com/files/thumbor/NBmkhh5uuTXxIP1w2AoPs48vTbk/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2014/04/08/814/n/1922441/dfba9a098f616cac_shutterstock_109357922/i/25-Top-World-Destinations-2014-TripAdvisor.jpg", 
+document.getElementById("img2").src = imagestwo[randomCount];  
+
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+var imagesthree=new Array
+imagesthree[1] = "https://aavietnamtravel.com/wp-content/uploads/2019/10/unnamed-4.jpg",
+imagesthree[2] = "https://i.pinimg.com/originals/af/0d/29/af0d29e8b6343451c7f9464ae9398129.jpg",
+imagesthree[3] = "https://www.avernausa.com/wp-content/uploads/2018/06/vacation-650x434.jpeg",  
+document.getElementById("img3").src = imagesthree[randomCount];
+  
+var imgCount = 3;
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+  var imagesfour=new Array
+imagesfour[1] = "https://i.pinimg.com/originals/42/bb/e5/42bbe59ef679dc6a97cbfb9ef154be49.jpg",
+imagesfour[2] = "https://news.expats.cz/wp-content/uploads/2019/03/iStock-831740990.jpg",
+imagesfour[3] = "https://travelwirenews.com/wp-content/uploads/2019/01/image-27.jpg",  
+document.getElementById("img4").src = imagesfour[randomCount];
+  
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+var imagesfive=new Array
+imagesfive[1] = "http://www.slovakia-maps.com/wp-content/uploads/2018/08/15.-Top-world-destinations.jpg",
+imagesfive[2] = "http://travelercorner.com/wp-content/uploads/2016/06/london-top-rated-destination.jpg",
+imagesfive[3] = "http://www.victoryinafrica.org/wp-content/uploads/2018/08/14.-Top-world-destinations.jpg",  
+document.getElementById("img5").src = imagesfive[randomCount]; 
+  
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+ var imagessix=new Array
+imagessix[1] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_168634214.jpg",
+imagessix[2] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1221929191.jpg",
+imagessix[3] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1406860232.jpg",  
+document.getElementById("img6").src = imagessix[randomCount];   
+  
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+ var imagesseven=new Array
+imagesseven[1] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_630570752.jpg",
+imagesseven[2] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_175331063.jpg",
+imagesseven[3] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1040847316.jpg",  
+document.getElementById("img7").src = imagesseven[randomCount];   
+  
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+  var imageseight=new Array
+imageseight[1] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_700484761.jpg",
+imageseight[2] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1038725650.jpg",
+imageseight[3] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_599713124.jpg",  
+document.getElementById("img8").src = imageseight[randomCount];   
+
+var imgCount = 3;
+
+var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
+  var imagesten=new Array
+imagesten[1] = "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_65988730.jpg",
+imagesten[2] = "https://www.thedubrovniktimes.com/media/k2/items/cache/29ce24a084a94893947f3fca54d7155f_XL.jpg",
+imagesten[3] = "https://coconuts.co/wp-content/uploads/2017/03/bali-1542569_640.jpg",  
+document.getElementById("img10").src = imagesten[randomCount];  
+}				
